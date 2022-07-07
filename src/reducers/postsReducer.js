d632b31795d144f5da637a4d8648abe2970c0fc3
lorrainetechnology <lorrainetechnology@gmail.com>
2022-07-07 13:33:37 +0530

    async redux

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'FETCH_POSTS':
+            return [...state,action.payload]
+        default:
+            return state
+    }
+}
